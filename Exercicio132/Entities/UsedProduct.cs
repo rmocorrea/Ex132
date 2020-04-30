51f5a65c352eaeffb5101109ef23b064128cf3c8
Romulo <bjj.rocorrea@gmail.com>
2020-04-30 11:44:18 -0300

    Primeiro commit

---
+++ Exercicio132/Entities/UsedProduct.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Exercicio132.Entities;
+using System.Globalization;
+
+namespace Exercicio132.Entities
+{
+    class UsedProduct : Product
+    {
+        public DateTime ManufactureDate { get; set; }
+
+        public UsedProduct()
+        {
+        }
+                      
+        public UsedProduct(string name, double price, DateTime manufactureDate)
+            : base(name, price)
+        {
+            ManufactureDate = manufactureDate;
+        }
+        public override string PriceTag()
+        {
+            return Name
+                + " (used) $"
+                + Price.ToString("F2", CultureInfo.InvariantCulture)
+                + " (ManufactureDate date: "
+                + ManufactureDate.ToString("dd/MM/yyyy")
+                + ")";
+        }
+    }
+}
